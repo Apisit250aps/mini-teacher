0bd2c453879f1ed89b137e7f3eb303fa87573600
YUapisit <aps.apisit250@gmail.com>
2026-02-11 11:38:15 +0700

    feat: implement UpdateYear function for year updates with validation and error handling

--- src/controllers/year.controller.ts
+++ src/controllers/year.controller.ts
@@ -1,8 +1,12 @@
 import { auth } from '@/auth'
 import { onErrorMessage, safeValidate } from '@/lib/utils'
-import { CreateYearSchema, Year } from '@/models/entities'
-import { createYear, getUniqYear } from '@/models/repositories'
+import { CreateYearSchema, UpdateYearSchema, Year } from '@/models/entities'
+import { createYear, getUniqYear, updateYear } from '@/models/repositories'
 import { NextRequest, NextResponse } from 'next/server'
+
+type YearParams = {
+  yearId: string
+}
 
 export async function CreateYear(
   req: NextRequest,
@@ -48,6 +52,52 @@ export async function CreateYear(
     return NextResponse.json(
       { success: true, message: 'Year created successfully', data: year },
       { status: 201 },
+    )
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Internal Server Error',
+        error: onErrorMessage(error),
+      },
+      { status: 500 },
+    )
+  }
+}
+
+export async function UpdateYear(
+  req: NextRequest,
+  { params }: { params: Promise<YearParams> },
+): Promise<NextResponse<ApiResponse<Year>>> {
+  try {
+    const session = await auth()
+    if (!session) {
+      return NextResponse.json(
+        { success: false, message: 'Unauthorized' },
+        { status: 401 },
+      )
+    }
+
+    const { yearId } = await params
+    const body = await req.json()
+    const validate = await safeValidate(UpdateYearSchema, body)
+    if (!validate.data) {
+      return NextResponse.json(
+        { success: false, message: 'Validation Error', errors: validate.error },
+        { status: 400 },
+      )
+    }
+
+    const update = await updateYear(yearId, validate.data)
+    if (!update) {
+      return NextResponse.json(
+        { success: false, message: 'Year not found' },
+        { status: 404 },
+      )
+    }
+    return NextResponse.json(
+      { success: true, message: 'Year updated successfully', data: update },
+      { status: 200 },
     )
   } catch (error) {
     return NextResponse.json(
