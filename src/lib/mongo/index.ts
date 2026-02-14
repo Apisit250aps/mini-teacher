6251878802429cad8826c18e2274443da46f0bdb
YUapisit <aps.apisit250@gmail.com>
2026-02-14 16:47:23 +0700

    feat: refactor usersCollection and update index creation for years and classes

--- src/lib/mongo/index.ts
+++ src/lib/mongo/index.ts
@@ -7,22 +7,6 @@ let _users: Collection<User> | null = null
 let _years: Collection<Year> | null = null
 let _classes: Collection<Class> | null = null
 
-export async function yearsCollection(): Promise<Collection<Year>> {
-  if (!_years) {
-    const db = await connect()
-    _years = db.collection<Year>('years')
-    await _years.createIndexes([
-      {
-        key: { user: 1, year: 1, term: 1 },
-        unique: true,
-        name: 'uniq_user_year_term',
-      },
-      { key: { id: 1 }, unique: true, name: 'uniq_id' },
-    ])
-  }
-  return _years
-}
-
 export async function usersCollection(): Promise<Collection<User>> {
   if (!_users) {
     const db = await connect()
@@ -35,12 +19,33 @@ export async function usersCollection(): Promise<Collection<User>> {
   return _users
 }
 
+export async function yearsCollection(): Promise<Collection<Year>> {
+  if (!_years) {
+    const db = await connect()
+    _years = db.collection<Year>('years')
+    await _years.createIndexes([
+      {
+        key: { user: 1, year: 1, term: 1 },
+        unique: true,
+        name: 'uniq_user_year_term',
+      },
+      { key: { id: 1 }, unique: true, name: 'uniq_id' },
+      { key: { user: 1 }, name: 'idx_user' },
+      { key: { year: 1 }, name: 'idx_year' },
+      { key: { term: 1 }, name: 'idx_term' },
+    ])
+  }
+  return _years
+}
+
 export async function classesCollection(): Promise<Collection<Class>> {
   if (!_classes) {
     const db = await connect()
     _classes = db.collection<Class>('classes')
     await _classes.createIndexes([
       { key: { id: 1 }, unique: true, name: 'uniq_id' },
+      { key: { name: 1 }, name: 'idx_name' },
+      { key: { year: 1 }, name: 'idx_year' },
     ])
   }
   return _classes
