6c10b1fe7ecd503b99bae4be5ce80f144e6d1685
YUapisit <aps.apisit250@gmail.com>
2026-02-15 10:39:56 +0700

    feat: add API endpoints for creating and retrieving class members with validation and authorization

--- src/app/api/class/[classId]/member/route.ts
+++ src/app/api/class/[classId]/member/route.ts
@@ -1,1 +1,3 @@
+export { GetMemberByClassId as GET } from '@/controllers'
 export { PatchClassMember as PUT } from '@/controllers'
+export { CreateAndAddClassMember as POST } from '@/controllers'
--- src/controllers/class-member.controller.ts
+++ src/controllers/class-member.controller.ts
@@ -1,8 +1,16 @@
+import { auth } from '@/auth'
 import { onErrorMessage, safeValidate } from '@/lib/utils'
-import { ClassMember, ClassMemberSchema } from '@/models/entities'
+import {
+  ClassMember,
+  ClassMemberDetail,
+  ClassMemberSchema,
+  CreateStudentSchema,
+} from '@/models/entities'
+import { createStudent } from '@/models/repositories'
 import {
   addClassMember,
   deleteClassMember,
+  getClassMembersByClassId,
   getUniqMember,
 } from '@/models/repositories/class-member.repo'
 import { NextRequest, NextResponse } from 'next/server'
@@ -61,6 +69,132 @@ export async function PatchClassMember(
         success: false,
         error: onErrorMessage(error),
         message: 'Failed to process request',
+      },
+      {
+        status: 500,
+      },
+    )
+  }
+}
+
+export async function CreateAndAddClassMember(
+  req: NextRequest,
+  { params }: { params: Promise<ClassMemberParams> },
+): Promise<NextResponse<ApiResponse<ClassMember>>> {
+  try {
+    const { classId } = await params
+    const session = await auth()
+
+    if (!session) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: 'Unauthorized',
+          message: 'You must be logged in to perform this action',
+        },
+        {
+          status: 401,
+        },
+      )
+    }
+
+    const body = await req.json()
+
+    const studentValidate = await safeValidate(CreateStudentSchema, {
+      ...body,
+      teacher: session.user.id,
+    })
+
+    if (!studentValidate.data) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: studentValidate.error!,
+          message: 'Invalid request data',
+        },
+        {
+          status: 400,
+        },
+      )
+    }
+
+    const student = await createStudent(studentValidate.data)
+    if (!student) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: 'Failed to create student',
+          message: 'Failed to create student',
+        },
+        {
+          status: 500,
+        },
+      )
+    }
+    const memberValidate = await safeValidate(ClassMemberSchema, {
+      classId,
+      studentId: student.id,
+    })
+    if (!memberValidate.data) {
+      return NextResponse.json(
+        {
+          success: false,
+          error: memberValidate.error!,
+          message: 'Invalid request data',
+        },
+        {
+          status: 400,
+        },
+      )
+    }
+    const classMember = await addClassMember(memberValidate.data)
+    return NextResponse.json(
+      {
+        success: true,
+        data: classMember,
+        message: 'Class member added successfully',
+      },
+      {
+        status: 200,
+      },
+    )
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: onErrorMessage(error),
+        message: 'Failed to process request',
+      },
+      {
+        status: 500,
+      },
+    )
+  }
+}
+
+export async function GetMemberByClassId(
+  req: NextRequest,
+  { params }: { params: Promise<ClassMemberParams> },
+): Promise<NextResponse<ApiResponse<ClassMemberDetail[]>>> {
+  try {
+    const { classId } = await params
+    const members = await getClassMembersByClassId(classId)
+    return NextResponse.json(
+      {
+        success: true,
+        data: members,
+        message: 'Class members retrieved successfully',
+      },
+      {
+        status: 200,
+      },
+    )
+  } catch (error) {
+    return NextResponse.json(
+      {
+        success: false,
+        error: onErrorMessage(error),
+        message: 'Failed to retrieve class members',
       },
       {
         status: 500,
