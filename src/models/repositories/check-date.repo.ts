865a79a852064aa9d94d5cd2a08f37351bb78955
YUapisit <aps.apisit250@gmail.com>
2026-02-17 09:05:05 +0700

    feat: add check date and check student repositories with CRUD operations

---
+++ src/models/repositories/check-date.repo.ts
@@ -0,0 +1,77 @@
+import { checkDatesCollection } from '@/lib/mongo'
+import { CheckDate } from '@/models/entities'
+import { omit } from 'lodash'
+import { pipe } from 'zod'
+
+export async function createCheckDate(
+  checkDate: CheckDate,
+): Promise<CheckDate> {
+  const check_dates = await checkDatesCollection()
+  const result = await check_dates.insertOne(checkDate)
+  if (!result.acknowledged) {
+    throw new Error('Failed to create check date')
+  }
+  return checkDate
+}
+
+export async function updateCheckDate(
+  id: string,
+  checkDate: Partial<CheckDate>,
+): Promise<CheckDate> {
+  const check_dates = await checkDatesCollection()
+  const update = await check_dates.findOneAndUpdate(
+    { id },
+    { $set: omit(checkDate, ['id', 'createdAt']) },
+    { returnDocument: 'after', projection: { _id: 0 } },
+  )
+  if (!update) {
+    throw new Error('Failed to update check date')
+  }
+  return update
+}
+
+export async function deleteCheckDate(id: string): Promise<void> {
+  const check_dates = await checkDatesCollection()
+  const result = await check_dates.deleteOne({ id })
+  if (result.deletedCount === 0) {
+    throw new Error('Failed to delete check date')
+  }
+}
+
+export async function getCheckDateById(id: string): Promise<CheckDate | null> {
+  const check_dates = await checkDatesCollection()
+  return await check_dates.findOne({ id }, { projection: { _id: 0 } })
+}
+
+export async function getCheckDatesByClassId(
+  classId: string,
+): Promise<CheckDate[]> {
+  const check_dates = await checkDatesCollection()
+  const checked = await check_dates
+    .aggregate<CheckDate>([
+      { $match: { classId } },
+      {
+        $lookup: {
+          from: 'check_students',
+          localField: 'id',
+          foreignField: 'checkDateId',
+          as: 'checkStudents',
+          pipeline: [
+            { $project: { _id: 0 } },
+            {
+              $lookup: {
+                from: 'students',
+                localField: 'studentId',
+                foreignField: 'id',
+                as: 'student',
+                pipeline: [{ $project: { _id: 0 } }],
+              },
+            },
+          ],
+        },
+      },
+      { $project: { _id: 0 } },
+    ])
+    .toArray()
+  return checked
+}
