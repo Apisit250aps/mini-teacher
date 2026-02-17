4106741fba5a696864822d4a0de84777f84edecc
YUapisit <aps.apisit250@gmail.com>
2026-02-17 16:27:21 +0700

    feat: refactor layout component to include authentication and year handling

--- src/app/[year]/[term]/class/layout.tsx
+++ src/app/[year]/[term]/class/layout.tsx
@@ -1,15 +1,8 @@
-import { auth } from '@/auth'
 import DashboardLayout from '@/components/layouts/dashboard-layout'
-import EmptyPage from '@/components/share/empty/empty-page'
 import { ClassProvider } from '@/hooks/app/use-class'
-import { YearProvider } from '@/hooks/app/use-year'
-
-import { getYearsByAuthUser, getYearsByYearTerm } from '@/models/repositories'
-import { redirect } from 'next/navigation'
 
 export default async function Layout({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode
   params: Promise<{
@@ -17,35 +10,9 @@ export default async function Layout({
     term: string
   }>
 }>) {
-  const session = await auth()
-  if (!session) {
-    redirect('/login')
-  }
-
-  const { year, term } = await params
-
-  const activeYear = await getYearsByYearTerm(
-    parseInt(year),
-    parseInt(term),
-    session.user.id,
-  )
-
-  const years = await getYearsByAuthUser(session.user.id)
-
-  if (!activeYear) {
-    return (
-      <EmptyPage
-        title={'ไม่พบปีการศึกษา'}
-        description={'ไม่พบปีการศึกษาที่ตรงกับปีและเทอมที่เลือก'}
-      />
-    )
-  }
-
   return (
-    <YearProvider activeYear={activeYear} years={years}>
-      <ClassProvider>
-        <DashboardLayout>{children}</DashboardLayout>
-      </ClassProvider>
-    </YearProvider>
+    <ClassProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </ClassProvider>
   )
 }
---
+++ src/app/[year]/[term]/layout.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import EmptyPage from '@/components/share/empty/empty-page'
+import { auth } from '@/auth'
+import { YearProvider } from '@/hooks/app/use-year'
+import { getYearsByYearTerm, getYearsByAuthUser } from '@/models/repositories'
+import { forbidden } from 'next/navigation'
+
+export default async function Layout({
+  children,
+  params,
+}: {
+  children: React.ReactNode
+  params: Promise<{
+    year: string
+    term: string
+  }>
+}) {
+  const session = await auth()
+  if (!session) {
+    forbidden()
+  }
+
+  const { year, term } = await params
+
+  const activeYear = await getYearsByYearTerm(
+    parseInt(year),
+    parseInt(term),
+    session.user.id,
+  )
+
+  const years = await getYearsByAuthUser(session.user.id)
+
+  if (!activeYear) {
+    return (
+      <EmptyPage
+        title={'ไม่พบปีการศึกษา'}
+        description={'ไม่พบปีการศึกษาที่ตรงกับปีและเทอมที่เลือก'}
+      />
+    )
+  }
+  return (
+    <YearProvider activeYear={activeYear} years={years}>
+      {children}
+    </YearProvider>
+  )
+}
