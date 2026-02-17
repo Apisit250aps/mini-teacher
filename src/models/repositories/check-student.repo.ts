10a3449e2866cb80f163bdbbbb315c628b75644d
YUapisit <aps.apisit250@gmail.com>
2026-02-17 09:10:12 +0700

    feat: implement CRUD operations for check students in repository

---
+++ src/models/repositories/check-student.repo.ts
@@ -0,0 +1,60 @@
+import { checkStudentsCollection } from '@/lib/mongo'
+import { CheckStudent } from '../entities'
+import { omit } from 'lodash'
+
+export async function createCheckStudent(
+  data: CheckStudent,
+): Promise<CheckStudent> {
+  const check_students = await checkStudentsCollection()
+  const result = await check_students.insertOne(data)
+  if (!result.acknowledged) {
+    throw new Error('Failed to create check student')
+  }
+  return data
+}
+
+export async function updateCheckStudent(
+  id: string,
+  data: Partial<CheckStudent>,
+): Promise<CheckStudent> {
+  const check_students = await checkStudentsCollection()
+  const result = await check_students.findOneAndUpdate(
+    { id },
+    { $set: omit(data, ['id', 'createdAt']) },
+    { returnDocument: 'after', projection: { _id: 0 } },
+  )
+  if (!result) {
+    throw new Error('Failed to update check student')
+  }
+  return result
+}
+
+export async function deleteCheckStudent(id: string): Promise<void> {
+  const check_students = await checkStudentsCollection()
+  const result = await check_students.deleteOne({ id })
+  if (result.deletedCount === 0) {
+    throw new Error('Failed to delete check student')
+  }
+}
+
+export async function getCheckStudentById(
+  id: string,
+): Promise<CheckStudent | null> {
+  const check_students = await checkStudentsCollection()
+  const [result] = await check_students.aggregate<CheckStudent>([
+    { $match: { id } },
+    {
+      $lookup: {
+        from: 'students',
+        localField: 'studentId',
+        foreignField: 'id',
+        as: 'student',
+        pipeline: [{ $project: { _id: 0 } }],
+      },
+    },
+    { $unwind: '$student' },
+    { $project: { _id: 0 } },
+  ]).toArray()
+
+  return result || null
+}
