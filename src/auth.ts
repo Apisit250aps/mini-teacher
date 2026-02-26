00a36f3d5a6eeec9da16a32626e83b0c7c3d4ed1
YUapisit <aps.apisit250@gmail.com>
2026-02-26 15:42:53 +0700

    Refactor code structure for improved readability and maintainability

--- src/auth.ts
+++ src/auth.ts
@@ -1,86 +1,13 @@
 import NextAuth from 'next-auth'
-import Credentials from 'next-auth/providers/credentials'
-import { AuthUpdateUser, UserLogin } from '@/models/entities'
-import {
-  findWithObjectId,
-  oAuthCreateUser,
-  updateUser,
-} from '@/models/repositories'
-import { verify } from '@/lib/utils/encryption'
-import { usersCollection } from '@/lib/mongo'
 import Google from 'next-auth/providers/google'
-import { MongoDBAdapter } from '@auth/mongodb-adapter'
-import client from './lib/mongo/client'
-import { safeValidate } from './lib/utils'
-import z from 'zod'
+import { prisma } from './lib/prisma/client'
+import { PrismaAdapter } from '@auth/prisma-adapter'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: MongoDBAdapter(client),
+  adapter: PrismaAdapter(prisma),
   session: {
     strategy: 'jwt',
   },
-  providers: [
-    Google,
-    Credentials({
-      name: 'credentials',
-      credentials: {
-        name: { label: 'Name', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        const { name, password } = credentials as UserLogin
-        const users = await usersCollection()
-        const user = await users.findOne({ name })
-        if (!user) {
-          return null
-        }
-        const isValid = await verify(user.password, password)
-        if (!isValid || !user.isActive) {
-          return null
-        }
-        await updateUser(user.id, {
-          lastLoginAt: new Date(),
-          updatedAt: new Date(),
-        })
-        return user
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        if (z.uuid().safeParse(user.id).success) {
-          token.sub = user.id
-        } else {
-          const u = await findWithObjectId(user.id)
-          token.sub = u?.id
-        }
-        token.isActive = user.isActive
-        token.isTeacher = user.isTeacher
-        token.firstName = user.firstName
-        token.lastName = user.lastName
-        token.email = user.email
-      }
-      return token
-    },
-    async session({ session, token }) {
-      if (token) {
-        session.user.id = token.sub as string
-        session.user.isActive = token.isActive as boolean
-        session.user.isTeacher = token.isTeacher as boolean
-        session.user.firstName = token.firstName as string
-        session.user.lastName = token.lastName as string
-        session.user.email = token.email as string
-      }
-      return session
-    },
-  },
-  events: {
-    createUser: async ({ user }) => {
-      const validate = await safeValidate(AuthUpdateUser, {
-        name: user.name || '',
-      })
-      await oAuthCreateUser(user.id!, validate.data!)
-    },
-  },
+  providers: [Google],
+  callbacks: {},
 })
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -6,14 +6,9 @@ declare module 'next-auth' {
    * The shape of the user object returned in the OAuth providers' `profile` callback,
    * or the second parameter of the `session` callback, when using a database.
    */
-  interface User extends DefaultUser {
-    id: string
-
-    name: string
-    email?: string
-    image?: string
-    emailVerified?: Date | null
-
+  interface User  {
+  
+  
     isActive: boolean
     isTeacher: boolean
     firstName?: string
