9f5583a495a946c77910915d60d91941f75e601a
YUapisit <aps.apisit250@gmail.com>
2026-03-07 21:24:56 +0700

    refactor: remove createYear controller and related index file

--- src/controllers/year/index.ts
+++
@@ -1,9 +0,0 @@
-import type { NextAuthRequest } from 'next-auth'
-import createYear from './create'
-import { Prisma } from '@prisma'
-import type { NextResponse } from 'next/server'
-
-class YearController {
-  createYear = createYear
-}
-export default YearController
