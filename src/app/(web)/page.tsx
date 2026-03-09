e93cd0011f1ff2dacda0897b8cb47e3403f31c76
YUapisit <aps.apisit250@gmail.com>
2026-03-09 20:24:20 +0700

    feat: simplify empty state component by removing unused elements and comments

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -2,15 +2,7 @@ import { auth } from '@/auth'
 import { yearRepository } from '@/core/repositories'
 import YearCreateCard from '@/presentations/components/app/year/year-create-card'
 import YearIntroSelect from '@/presentations/components/app/year/year-intro-select'
-import {
-  Empty,
-  EmptyContent,
-  EmptyDescription,
-  EmptyHeader,
-  EmptyMedia,
-  EmptyTitle,
-} from '@/presentations/components/ui/empty'
-import { School } from 'lucide-react'
+import { Empty, EmptyContent } from '@/presentations/components/ui/empty'
 import { forbidden } from 'next/navigation'
 
 export default async function Home() {
@@ -26,16 +18,6 @@ export default async function Home() {
   return (
     <div className="h-full">
       <Empty className="bg-muted/30 h-dvh">
-        {/* <EmptyHeader>
-          <EmptyMedia variant="icon">
-            <School />
-          </EmptyMedia>
-          <EmptyTitle>ยินดีต้อนรับ</EmptyTitle>
-          <EmptyDescription className="max-w-2xl text-pretty">
-            สวัสดีครับ ยินดีต้อนรับสู่ระบบจัดการห้องเรียนของเรา!
-            เริ่มต้นด้วยการสร้างห้องเรียนใหม่หรือเข้าร่วมห้องเรียนที่มีอยู่แล้วเพื่อเริ่มต้นการเรียนรู้และการสอนอย่างมีประสิทธิภาพ
-          </EmptyDescription>
-        </EmptyHeader> */}
         <EmptyContent>
           {years.length === 0 ? (
             <YearCreateCard />
