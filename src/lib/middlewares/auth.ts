b619a16e8375728a7845487472e635d6e664c39d
YUapisit <aps.apisit250@gmail.com>
2026-03-12 09:44:08 +0700

    feat: remove unused authentication functions; update middleware configuration for improved security

--- src/lib/middlewares/auth.ts
+++ src/lib/middlewares/auth.ts
@@ -1,23 +1,15 @@
 import { NextAuthRequest } from 'next-auth'
-import { forbidden } from 'next/navigation'
 import { NextResponse } from 'next/server'
-
-export const isNoAuth = async (req: NextAuthRequest) => {
-  const session = req.auth
-  if (session) {
-    return NextResponse.redirect(new URL('/', req.nextUrl))
-  }
-}
-
-export const signAuth = async (req: NextAuthRequest) => {
-  const session = req.auth?.user
-  req.user = session
-}
 
 export const isAuthenticated = async (req: NextAuthRequest) => {
   const session = req.auth
-
   if (!session) {
-    return forbidden()
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Unauthorized',
+      },
+      { status: 401 },
+    )
   }
 }
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,17 +1,31 @@
 import { auth } from '@/auth'
 import { useMiddlewares } from '@/lib/middlewares'
-import { signAuth } from './lib/middlewares/auth'
-import { debugMiddlewares } from './lib/middlewares/app';
+import { isAuthenticated } from './lib/middlewares/auth'
+import { debugMiddlewares } from './lib/middlewares/app'
 
 export default auth((req) =>
   useMiddlewares(req, [
     {
       prefix: '/',
-      middlewares: [debugMiddlewares, signAuth],
+      middlewares: [debugMiddlewares],
+    },
+    {
+      prefix: '/api',
+      middlewares: [isAuthenticated],
     },
   ]),
 )
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    /*
+     * 1. Apply to all paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * 2. Exclude /api/auth in all formats (e.g., /api/auth/signin, /api/auth/callback)
+     * 3. But still apply to all other /api routes
+     */
+    '/((?!api/auth|_next/static|_next/image|favicon.ico).*)',
+  ],
 }
