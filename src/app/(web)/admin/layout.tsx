25c5d451a81e1ebb2be3a49c6d69b27fc8f75b0b
YUapisit <aps.apisit250@gmail.com>
2026-03-12 15:59:33 +0700

    feat: implement admin access control in layout component

--- src/app/(web)/admin/layout.tsx
+++ src/app/(web)/admin/layout.tsx
@@ -1,6 +1,16 @@
+import { auth } from '@/auth'
 import AdminLayout from '@/presentations/components/layouts/admin-layout'
+import { forbidden } from 'next/navigation'
 import React from 'react'
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const session = await auth()
+  if (!session?.user?.isAdmin) {
+    return forbidden()
+  }
   return <AdminLayout>{children}</AdminLayout>
 }
